2f1cb8ece8fdd93c8c4b977ea58c7144aa64a944
Yash <yashuan0202@gmail.com>
2023-11-01 21:14:15 +0530

    Add files via upload

---
+++ Ch.7 Loops/break_statement.py
@@ -0,0 +1,9 @@
+for i in range (20):
+    print(i)
+    if i == 15:
+        break
+
+'''agar hum ek (=) lagayenge toh vo i ki value 
+assign ho jaayegi, isliye hum double (==) lagate 
+hain, iska matlab jab i ki value 15 pe ho jaye
+tab statement break karni hai'''
---
+++ Ch.7 Loops/pass_statement.py
@@ -0,0 +1,17 @@
+# Pass is a null statement in Python
+# It instructs to "Do Nothing"
+
+for i in range (10):
+    pass
+
+def Function1():
+    pass
+
+i=10
+if i>10:
+    pass
+
+while i>9:
+    pass
+
+print("My name is Yash")
