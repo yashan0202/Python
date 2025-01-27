7d3abc8d551544612fd23f83c1786ae90a716547
Yash <yashuan0202@gmail.com>
2025-01-27 20:51:02 +0530

    Update 09_tuple_method.py

--- 00_Chapters/Ch.4 List & Tuples/09_tuple_method.py
+++ 00_Chapters/Ch.4 List & Tuples/09_tuple_method.py
@@ -3,3 +3,25 @@ tuple = (1, 2, 4, 5, 19, 1, 1, 1, 19)
 
 print(tuple.count(1))
 print(tuple.index(5))
+
+t = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,10, 3.14)
+
+print(t[:9:2])
+
+print(len(t))
+
+print(t.count(10))
+
+print(any(t))
+
+print(min(t))
+
+print(max(t))
+
+print(sum(t))
+
+print(sorted(t))
+
+print(t.index(10))
+
+print(t.index(3.14))
